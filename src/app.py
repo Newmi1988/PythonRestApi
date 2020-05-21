54ba2e6afcd2cae2379cb3e52b3593c56d4b9aa6
Newmi1988 <newmi1988@gmail.com>
2020-05-21 14:25:05 +0200

    switched from flask-restful to flask-restx

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restx import Api
 
 app = Flask(__name__)
 api = Api(app = app)
 
 from .resources.endpoint import Endpoint
 
-api.add_resource(Endpoint, '/')
+api.add_resource(Endpoint)
 
--- src/resources/endpoint.py
+++ src/resources/endpoint.py
@@ -1,5 +1,8 @@
-from flask_restful import Resource
+from flask_restx import Resource
+from ..app import api
 
+
+@api.route('/')
 class Endpoint(Resource):
     def get(self):
         return {'api_version' : '0.0.1'}
