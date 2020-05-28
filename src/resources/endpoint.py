038ad8acd9ff2792e58cd8546ca77b492a18e82b
Tobias Newmiwaka <newmi1988@gmail.com>
2020-05-28 20:02:01 +0200

    added non root endpoint

--- src/resources/endpoint.py
+++ src/resources/endpoint.py
@@ -2,7 +2,7 @@ from flask_restx import Resource
 from ..app import api
 
 
-@api.route('/')
+@api.route('/api')
 class Endpoint(Resource):
     def get(self):
         return {'api_version' : '0.0.1'}
